b96ea5a96b3ba8f39deebaf7a002cc54087d1f67
Sebastian Duarte <Subsono33@gmail.com>
2022-05-11 02:20:53 -0400

    finish rough v app

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const DEFAULT_PLACEHOLDER_IMAGE ="https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-export const Movie = ({ movie }) => {
+ const Movie = ({ movie }) => {
   const poster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
   return (
-    <div> className="movie"
+    <div className="movie"> 
       <h2> {movie.Title}</h2>
       <div>
         <img
@@ -18,4 +18,6 @@ export const Movie = ({ movie }) => {
     
     </div>
   )
-}
+ }
+
+ export default Movie
