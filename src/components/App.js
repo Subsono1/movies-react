017858fa9e42139b981ecd0940cde04f2d11c7e2
Sebastian Duarte <Subsono33@gmail.com>
2022-05-10 23:54:11 -0400

    created components, app, header and some css

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      HI how are you this is React. 
-    </div>
-  );
+  return <div>HI how are you this is React.</div>;
 }
 
 export default App;
